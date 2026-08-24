1340e958c7db0aee243f045a231f206833ccb72c
droid <v@gmail.com>
2026-08-24 21:30:48 +0500

    static css, featured app

--- main/views.py
+++ main/views.py
@@ -4,8 +4,12 @@ from .models import App
 
 
 def index(request):
-    apps = App.objects.all()
-    return render(request, 'main/index.html', {'apps': apps})
+    apps = App.objects.order_by('-created_at').all()
+    featured = App.objects.order_by('-price').first()
+    return render(request, 'main/index.html', {
+        'apps': apps,
+        'featured': featured,
+    })
 
 
 def about(request):
