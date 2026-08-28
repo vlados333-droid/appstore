c830476d6d2cb1677d8f56213c65f663c61f8134
droid <v@gmail.com>
2026-08-28 21:30:17 +0500

    category page

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('app/<int:app_id>/', views.app_detail, name='app_detail'),
+    path('category/<int:category_id>/', views.category_detail, name='category'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-from .models import App
+from .models import App, Category
 
 
 def index(request):
     apps = App.objects.order_by('-created_at').all()
     featured = App.objects.order_by('-price').first()
+    categories = Category.objects.all()
     return render(request, 'main/index.html', {
         'apps': apps,
         'featured': featured,
+        'categories': categories,
     })
 
 
@@ -20,3 +22,12 @@ def about(request):
 def app_detail(request, app_id):
     app = get_object_or_404(App, id=app_id)
     return render(request, 'main/app_detail.html', {'app': app})
+
+
+def category_detail(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    apps = App.objects.filter(category=category)
+    return render(request, 'main/category.html', {
+        'category': category,
+        'apps': apps,
+    })
